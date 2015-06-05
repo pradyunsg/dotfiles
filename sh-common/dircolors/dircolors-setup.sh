aea2cd838d58a55fca0a1624e4e2e5ccae766355
Pradyun <pradyunsg@gmail.com>
2015-06-05 11:52:05 +0530

    [BASH, COMMON] Remove redundant files

--- sh-common/dircolors/dircolors-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIRCOLORS_FILE=$HOME/.bash/dircolors/rainbow.dircolors
-# Uncomment next line when developing scheme...
-# python3.4 $HOME/.bash/dircolors/rainbow_dircolors.py > $DIRCOLORS_FILE
-eval $(dircolors $DIRCOLORS_FILE)
