5f5bbe9cd761aee8572b9e8f5187859e94bd0ec4
Pradyun <pradyunsg@gmail.com>
2016-01-09 19:27:01 +0530

    :fire: Remove all antigen sources

--- sh-zsh/antigen/tests/theme.t
+++
@@ -1,5 +0,0 @@
-Load the theme explicitly.
-
-  $ antigen-theme $PLUGIN_DIR silly &> /dev/null
-  $ echo "$PS1"
-  prompt>
--- sh-zsh/antigen/travis-ppa.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-if [ $PPA != 'none' ];then
-    sudo apt-add-repository -y $PPA;
-    sudo apt-get update -qq;
-fi
