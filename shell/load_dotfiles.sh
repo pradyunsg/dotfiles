4caeb7c661207c928b55595fe9eb77db7764c453
Pradyun <pradyunsg@gmail.com>
2016-03-21 14:55:13 +0530

    [SHELL] Switch to a different loading mechanism
    BREAKING CHANGE: The repo becomes non-functional due to this change
    unless things are renamed or such.
    
    Require all the files to end in shrc or the name of the shell if they
    are to be loaded on startup. Takes code from @holman's dotfiles, to
    determine the loading order. Packages will be ported incrementally.

---
+++ shell/load_dotfiles.sh
@@ -0,0 +1,31 @@
+# Loads all shell files to be sourced from the dotfiles
+# Largely taken from @holman's dotfiles
+
+typeset -U config_files
+
+config_files=($DOTFILES_LOCATION/**/*.(shrc|${CURRENT_SHELL}))
+
+# load the path files
+for file in ${(M)config_files:#*/path.(shrc|${CURRENT_SHELL})}
+do
+  source $file
+done
+
+# load everything but the path and completion files
+for file in ${${config_files:#*/path.(shrc|${CURRENT_SHELL})}:#*/completion.(shrc|${CURRENT_SHELL})}
+do
+  source $file
+done
+
+# initialize autocomplete here, otherwise functions won't be loaded
+autoload -U compinit
+compinit
+
+# load every completion after autocomplete loads
+for file in ${(M)config_files:#*/completion.(shrc|${CURRENT_SHELL})}
+do
+  source $file
+done
+
+unset config_files
+unset source
