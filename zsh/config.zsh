a80c26b0fe0cf7043e6947e90e9365fee3a63f6d
Pradyun <pradyunsg@gmail.com>
2016-04-25 12:02:34 +0530

    [ZSH] Improve history and completion behaviour

---
+++ zsh/config.zsh
@@ -0,0 +1,7 @@
+# CONFIG:: Extra ZSH configuration variables
+# How often to auto-update (in days).
+UPDATE_ZSH_DAYS=1
+# Command auto-correction.
+ENABLE_CORRECTION="true"
+# display red dots whilst waiting for completion.
+COMPLETION_WAITING_DOTS="true"
