4de93390fda456e237cbbc3f230b7a5e062b2bf8
Pradyun S. Gedam <pradyunsg@gmail.com>
2016-12-05 11:44:05 +0530

    [BOOTSTRAP] Improve bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,21 +6,23 @@
 
 SCRIPT_DIR=$( cd $(dirname $0) ; pwd )
 
-# Check if Python/Pip 3 is installed. . If that fails, abort.
-which pip3 > /dev/null
-
-# If not installed, then install it, via apt
-if [ $? != "0" ]; then
-    sudo apt-get update
-    echo "Installing Python 3 and pip."
-    sudo apt-get install --yes python3 python3-pip
+function have_command() {
+    which $1 > /dev/null
     if [ $? != "0" ]; then
-        echo "Could not install Python 3. Please install Python 3 (with pip) and re-run."
-        exit 1
+        echo false
+    else
+        echo true
     fi
+}
+
+# If we don't have pip or python 3.4+ available
+if [[ $(have_tool python3) == "false" || $(have_tool pip3) == "false" ]]; then
+    echo "Please install Python 3.4+ and pip 8.2.0+ to continue."
+    exit 1
 fi
 
-echo "Installing click for the user..."
-pip3 install --user click==6.2.0
+echo "Installing click==6.6 for manage script"
+pip3 install click==6.6
 
 python3 manage setup_new_system
+python3 manage sync
