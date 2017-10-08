a2706d31cafd620d6c8848d9279defd8b678c229
Pradyun Gedam <pradyunsg@gmail.com>
2017-10-08 18:39:08 +0530

    [OSX] Move darwin specific config out

---
+++ zsh/mac-config.zsh
@@ -0,0 +1,6 @@
+# This script only for when I use my MacBook.
+# It describes all the configuration variables for things.
+[[ "$OSTYPE" != darwin* ]] && return
+
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
