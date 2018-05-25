635c014536f64e65c643d83b18537cd29cf1b0d3
Pradyun Gedam <pradyunsg@gmail.com>
2018-05-25 19:32:05 +0530

    Move virtualenvwrapper export to python

--- zsh/mac-config.zsh
+++
@@ -1,6 +0,0 @@
-# This script only for when I use my MacBook.
-# It describes all the configuration variables for things.
-[[ "$OSTYPE" != darwin* ]] && return
-
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-export VIRTUALENVWRAPPER_SCRIPT=/usr/local/bin/virtualenvwrapper.sh
