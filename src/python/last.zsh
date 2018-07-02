e063c354623ff4d9c307085f4601c3444d83d2eb
Pradyun Gedam <pradyunsg@gmail.com>
2018-07-02 13:54:08 +0530

    Start using homebrew pyenv
    Had to downgrade to 3.6.5 since 3.6.6 can't be installed using any
    released pyenv version. :(

--- src/python/last.zsh
+++ src/python/last.zsh
@@ -1,2 +1,2 @@
 eval "$(pyenv init -)"
-pyenv shell 3.7.0 3.6.6 3.5.5 3.4.8 2.7.15
+pyenv shell 3.7.0 3.6.5 3.5.5 3.4.8 2.7.15
--- src/python/pyenv.zsh
+++
@@ -1,2 +0,0 @@
-export PYENV_ROOT="$HOME/.pyenv"
-add_to_path "$PYENV_ROOT/bin"
