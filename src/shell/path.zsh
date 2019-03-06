b972fc56c5bd782204cfbee5626e266842068a4d
Pradyun Gedam <pradyunsg@gmail.com>
2019-03-06 10:50:04 +0530

    Add /usr/local/sbin

--- src/shell/path.zsh
+++ src/shell/path.zsh
@@ -1,3 +1,8 @@
+# set PATH so it includes system wide sbin if it exists
+if [ -d "/usr/local/sbin" ] ; then
+    PATH="/usr/local/sbin:$PATH"
+fi
+
 # set PATH so it includes user's private bin if it exists
 if [ -d "$HOME/bin" ] ; then
     PATH="$HOME/bin:$PATH"
