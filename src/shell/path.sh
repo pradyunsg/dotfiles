ec54aac3be5d53851f413041ed77475c1a04052f
Pradyun Gedam <pradyunsg@gmail.com>
2019-03-06 10:49:41 +0530

    Move PATH addition from profile to dotfiles-load

---
+++ src/shell/path.sh
@@ -0,0 +1,9 @@
+# set PATH so it includes user's private bin if it exists
+if [ -d "$HOME/bin" ] ; then
+    PATH="$HOME/bin:$PATH"
+fi
+
+# set PATH so it includes user's local bin if it exists
+if [ -d "$HOME/.local/bin" ] ; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
