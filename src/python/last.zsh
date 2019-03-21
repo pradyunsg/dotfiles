e42b238643989fd8565db20fdc2f60232e05e8fb
Pradyun Gedam <pradyunsg@gmail.com>
2019-03-21 19:33:34 +0530

    :horse: Lazy load pyenv and virtualenvwrapper

--- src/python/last.zsh
+++ src/python/last.zsh
@@ -1,3 +1,4 @@
-eval "$(pyenv init -)"
-pyenv shell 3.7.1 3.6.7 3.5.6 3.4.9 2.7.15
-pyenv virtualenvwrapper_lazy
+# Export variable instead of calling pyenv to minimize startup time.
+export PYENV_VERSIONS=3.7.1:3.6.7:3.5.6:3.4.9:2.7.15
+# Load lazily to minimize startup time.
+virtualenvwrapper_lazy.sh
