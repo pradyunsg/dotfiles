a0c98b84501bbd47e6e8d4d7162b97c6b811b0a3
Pradyun Gedam <pradyunsg@gmail.com>
2019-03-25 13:41:38 +0530

    Improve logic for lib/

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,5 +1,1 @@
 from .cli import main
-
-
-if __name__ == '__main__':
-    main()
---
+++ lib/__main__.py
@@ -0,0 +1,8 @@
+"""An alternative entry point
+"""
+
+from lib.cli import main
+
+
+if __name__ == '__main__':
+    main()
