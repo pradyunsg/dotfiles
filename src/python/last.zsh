eb8f8c1a3b140d6e9fbe27ee60865c9cd41256b4
Pradyun Gedam <pradyunsg@gmail.com>
2019-03-25 13:43:12 +0530

    HARDCODED enabling of virtualenvwrapper_lazy

--- src/python/last.zsh
+++ src/python/last.zsh
@@ -1,4 +1,6 @@
 # Export variable instead of calling pyenv to minimize startup time.
 export PYENV_VERSIONS=3.7.1:3.6.7:3.5.6:3.4.9:2.7.15
+
 # Load lazily to minimize startup time.
-virtualenvwrapper_lazy.sh
+export VIRTUALENVWRAPPER_SCRIPT=/Users/pradyunsg/.pyenv/versions/3.7.1/bin/virtualenvwrapper.sh
+source /Users/pradyunsg/.pyenv/versions/3.7.1/bin/virtualenvwrapper_lazy.sh
