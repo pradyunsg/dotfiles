d80c2cb8287a832dc1ed9e5a3a3512f2f7f82e43
Pradyun Gedam <pradyunsg@gmail.com>
2019-05-24 22:38:35 -0400

    Change defaults for sigma-prompt

--- src/prompt/last.zsh
+++ src/prompt/last.zsh
@@ -3,7 +3,7 @@ _pradyunsg_log "Enabling prompt..."
 PROMPT_DESIGN=${PROMPT_DESIGN:=powerline}
 PROMPT_THEME=${PROMPT_THEME:=pradyunsg}
 
-SIGMA_PROMPT_LOCATION=${SIGMA_PROMPT_LOCATION:=~/code/sigma-prompts}
+SIGMA_PROMPT_LOCATION=${SIGMA_PROMPT_LOCATION:=~/Projects/sigma-prompts}
 
 if [[ -f "${SIGMA_PROMPT_LOCATION}/prompt.sh" ]]; then
     source "${SIGMA_PROMPT_LOCATION}/prompt.sh"
