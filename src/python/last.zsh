052d6d9199585f3f69f3d0a59a99cebc1c8f498f
Pradyun Gedam <pradyunsg@gmail.com>
2019-11-05 17:08:20 +0530

    Python 3.8!

--- src/python/last.zsh
+++ src/python/last.zsh
@@ -1,5 +1,5 @@
 # Export variable instead of calling pyenv to minimize startup time.
-export PYENV_VERSION=3.7.4:3.6.9:3.5.7:2.7.16
+export PYENV_VERSION=3.8.0:3.7.4:3.6.9:3.5.7:2.7.16
 
 # Setup virtualenvwrapper
 pyenv virtualenvwrapper_lazy
