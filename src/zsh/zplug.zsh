c39381ccfad43a1b854213f8fa8af982b8c4d460
Pradyun Gedam <pradyunsg@gmail.com>
2020-01-17 13:45:54 +0530

    Cleanup unused zplug items

--- src/zsh/zplug.zsh
+++ src/zsh/zplug.zsh
@@ -4,23 +4,10 @@ source $ZPLUG_HOME/init.zsh
 
 command -v zplug >/dev/null 2>&1 || return
 
-# zplug "plugins/pip", from:oh-my-zsh
-# zplug "plugins/python", from:oh-my-zsh
-# zplug "plugins/compleat", from:oh-my-zsh
-# zplug "plugins/rbenv", from:oh-my-zsh
-# zplug "plugins/rust", from:oh-my-zsh
-# zplug "plugins/heroku", from:oh-my-zsh
-# zplug "plugins/vagrant", from:oh-my-zsh
-
-zplug "djui/alias-tips", from:github
-# zplug "supercrabtree/k", from:github
-zplug "Tarrasch/zsh-autoenv", from:github
 zplug "zsh-users/zsh-autosuggestions", from:github
 zplug "zsh-users/zsh-syntax-highlighting", from:github
 zplug "romkatv/zsh-prompt-benchmark", from:github
 zplug "romkatv/powerlevel10k", as:theme, depth:1
-# zplug "mafredri/zsh-async", from:github
-# zplug sindresorhus/pure, use:pure.zsh, from:github, as:theme
 
 # If the plugins are not installed, install them
 if ! zplug check; then
