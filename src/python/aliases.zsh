0bcccc9f416b8765fb03d1448e6547a01d78af29
Pradyun Gedam <pradyunsg@gmail.com>
2020-07-05 06:10:14 +0530

    Allow passing version in mkpkg()

--- src/python/aliases.zsh
+++ src/python/aliases.zsh
@@ -1,21 +1,21 @@
 # Sometimes, you need a package quickly
 function mkpkg() {
-    if [ -n "$1" ]; then
-        mkdir -p "./$1/$1"
-        echo "\
+    if [ -z "$2" ]; then
+        echo "Need two arguments"
+        return 1
+    fi
+    mkdir -p "./$1/$1"
+
+    echo "\
 from setuptools import setup, find_packages
 
 setup(
     name=\"$1\",
-    version=\"0.1.0\",
+    version=\"$2\",
     packages=find_packages()
 )
 " > "./$1/setup.py"
-        echo "print('Hi from $1')" > "./$1/$1/__init__.py"
-        return 0
-    fi
-    echo "Need an argument"
-    return 1
+    echo "print('Hi from $1 $2')" > "./$1/$1/__init__.py"
 }
 
 function toxr() {
