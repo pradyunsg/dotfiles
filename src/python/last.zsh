b9bc7d1ae89ee8290f07863c960e37ea88ae8b2e
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-07-25 13:28:29 +0530

    Update to newer pyenv versions!

--- src/python/last.zsh
+++ src/python/last.zsh
@@ -1,5 +1,5 @@
 # Export variable instead of calling pyenv to minimize startup time.
-export PYENV_VERSION=3.8.0:3.7.5:3.6.9:3.5.7:2.7.16
+export PYENV_VERSION=3.8.3:3.7.7:3.6.10:3.5.9:2.7.18
 
 # Setup virtualenvwrapper
 eval "$(pyenv init -)"
