0856d507d6c266156e7a74025e4cb92f164bf9e2
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2020-12-18 11:59:22 +0000

    Drop virtualenvwrapper from pyenv

--- src/python/last.zsh
+++
@@ -1,6 +0,0 @@
-# Export variable instead of calling pyenv to minimize startup time.
-export PYENV_VERSION=3.8.3:3.7.7:3.6.10:3.5.9:2.7.18
-
-# Setup virtualenvwrapper
-eval "$(pyenv init -)"
-pyenv virtualenvwrapper_lazy
---
+++ src/python/pyenv.zsh
@@ -0,0 +1,2 @@
+export PYENV_VERSION=3.8.3:3.7.7:3.6.10:3.5.9:2.7.18
+eval "$(pyenv init -)"
