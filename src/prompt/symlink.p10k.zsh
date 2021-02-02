ddb43bd1a814b936495b614bcd5db0aacc16316b
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-02-02 12:18:05 +0000

    Add a prompt gap character

--- src/prompt/symlink.p10k.zsh
+++ src/prompt/symlink.p10k.zsh
@@ -162,7 +162,7 @@
   # the number of prompt lines. You'll probably want to set POWERLEVEL9K_SHOW_RULER=false
   # if using this. You might also like POWERLEVEL9K_PROMPT_ADD_NEWLINE=false for more compact
   # prompt.
-  typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR=' '
+  typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR='·'
   if [[ $POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_CHAR != ' ' ]]; then
     # The color of the filler.
     typeset -g POWERLEVEL9K_MULTILINE_FIRST_PROMPT_GAP_FOREGROUND=242
