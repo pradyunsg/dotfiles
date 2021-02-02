116d7af973cbbde186d512e967a73d65b348dc1d
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-02-02 12:42:48 +0000

    Move the brew+pyenv alias to pyenv-specific file

--- src/python/pyenv.zsh
+++ src/python/pyenv.zsh
@@ -1,4 +1,8 @@
 # Initialise up pyenv, if it's available
 if command -v pyenv --version > /dev/null 2>&1; then
   eval "$(pyenv init -)"
+
+  # For pyenv and "brew doctor" working together well.
+  # See https://github.com/pyenv/pyenv/issues/106
+  alias brew="env PATH=${PATH//$(pyenv root)\/shims:/} brew"
 fi
--- src/shell/aliases.sh
+++ src/shell/aliases.sh
@@ -37,7 +37,3 @@ fi
 # Because it's shorter
 # -----------------------------------------------------------------------------
 alias g="git"
-
-# For pyenv and "brew doctor" working together well.
-# See https://github.com/pyenv/pyenv/issues/106
-alias brew="env PATH=${PATH//$(pyenv root)\/shims:/} brew"
