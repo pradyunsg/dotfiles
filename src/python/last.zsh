2dc25bdf9b5fa76e90a4918f1d867e7d66640956
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-02-05 09:19:21 +0000

    Let me know that the virtualenv is being activated

--- src/python/last.zsh
+++ src/python/last.zsh
@@ -2,6 +2,8 @@ if [[ "$OSTYPE" == darwin* ]]; then
   export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.8/bin
 fi
 
+# Activate virtualenv if in current directory
 if [ -d .venv ]; then
+  echo "Activating virtualenv..."
   v a
 fi
