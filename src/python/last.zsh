6f02787f4d2d4828e00ea580068594203f24b65b
Pradyun Gedam <pradyunsg@users.noreply.github.com>
2021-07-08 20:57:22 +0100

    Get Homebrew working, for realsies

--- src/python/last.zsh
+++ src/python/last.zsh
@@ -1,7 +1,3 @@
-if [[ "$OSTYPE" == darwin* ]]; then
-  export PATH=/Library/Frameworks/Python.framework/Versions/3.8/bin:$PATH
-fi
-
 # Activate virtualenv if in current directory
 if [ -d .venv ]; then
   echo "Activating virtualenv..."
