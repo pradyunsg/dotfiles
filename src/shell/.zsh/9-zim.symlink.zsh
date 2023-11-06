6b450bd8a1f9e2858f61ae0299a39ad0e4b1ab07
Pradyun Gedam <pgedam@bloomberg.net>
2023-11-06 12:48:51 -0500

    Move from zplug to zim
    This is a better plugin manager that seems to be less fragile.

---
+++ src/shell/.zsh/9-zim.symlink.zsh
@@ -0,0 +1,14 @@
+ZIM_HOME=~/.zim
+
+# Download zimfw plugin manager if missing.
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh \
+      https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+fi
+
+# Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+  source ${ZIM_HOME}/zimfw.zsh init -q
+fi
+
+source ${ZIM_HOME}/init.zsh
